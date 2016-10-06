3dfdc3f2a2225d6f99e3a39489c5b268300df5d7
croftsr <cro11007@byui.edu>
2016-10-06 13:35:01 -0600

    The real thing this time (and the addition to Player)

--- BYUI-Do/src/byui/pkgdo/BYUIDo.java
+++ BYUI-Do/src/byui/pkgdo/BYUIDo.java
@@ -5,12 +5,58 @@
  */
 package byui.pkgdo;
 
+import java.util.Objects;
+
 /**
  *
  * @author Scotty
  */
-public class BYUIDo {
+public class BYUIDo implements Serializable{
 
+    //class instance variables
+    private String name;
+
+    public BYUIDo() {
+    }
+ 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 79 * hash + Objects.hashCode(this.name);
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "BYUIDo{" + "name=" + name + '}';
+    }
+    
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final BYUIDo other = (BYUIDo) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }    
     /**
      * @param args the command line arguments
      */
