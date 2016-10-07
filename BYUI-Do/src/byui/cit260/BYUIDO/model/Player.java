eebccbf1a0ecb750ef8517f97319ade39675857f
Nick <Nick@LAPTOP-MFGV8EVG>
2016-10-07 13:35:42 -0600

    10-05-2016 fixed the ish we just effed up

--- BYUI-Do/src/byui/cit260/BYUIDO/model/Player.java
+++ BYUI-Do/src/byui/cit260/BYUIDO/model/Player.java
@@ -5,10 +5,59 @@
  */
 package byui.cit260.BYUIDO.model;
 
+import java.io.Serializable;
+import java.util.Objects;
+
 /**
  *
  * @author Scotty
  */
-public class Player {
+public class Player implements Serializable{
+    
+    private String name;
+
+    public Player() {
+    }
+    
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 71 * hash + Objects.hashCode(this.name);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Player other = (Player) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" + "name=" + name + '}';
+    }
+    
+         
     
 }
