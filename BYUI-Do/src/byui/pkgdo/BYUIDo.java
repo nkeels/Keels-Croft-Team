3b0de6d5d56c04f8aa46c455f3068c87c4783f2a
Nick <Nick@LAPTOP-MFGV8EVG>
2016-10-07 22:57:35 -0600

    10-07-2016 I promise this time

--- BYUI-Do/src/byui/pkgdo/BYUIDo.java
+++ BYUI-Do/src/byui/pkgdo/BYUIDo.java
@@ -26,66 +26,47 @@ public class BYUIDo implements Serializable {
 
     public static void main(String[] args) {
 
-        //set all values for classes
+        
         Player playerOne = new Player();
         playerOne.setName("Nicholas Cage");
         System.out.println("\nPlayer one:" + playerOne.toString());
 
         Character zelda = new Character();
         zelda.setCharName("Katy Perry");
+        zelda.setStress(180);
+        zelda.setConfide(25);
+        zelda.setGender("Male");
+        zelda.setPersonalityQual(79);
         System.out.println("\nCharacter Name: " + zelda.toString());
 
-        Character stress = new Character();
-        stress.setStress(180);
-        System.out.println("\nStress Level: " + stress.toString());
-
-        Character confide = new Character();
-        confide.setConfide(25);
-        System.out.println("\nConfidence level:" + confide.toString());
-
-        Character gender = new Character();
-        gender.setGender("Male");
-        System.out.println("\nGender:" + gender.toString());
-
-        Character persona = new Character();
-        persona.setPersonalityQual(79);
-        System.out.println("\nQuality of Personality:" + persona.toString());
 
         Game bYUIdo = new Game();
         bYUIdo.setPlayerName("Rachel McAdams");
+        bYUIdo.setConfirmation("Yes");
         System.out.println("\nGame File Name:" + bYUIdo.toString());
 
-        Game confirm = new Game();
-        confirm.setConfirmation("Yes");
-        System.out.println("\nDo you wish to continue? " + confirm.toString());
 
         Location location = new Location();
         location.setTitle("Tuscany Apartments");
+        location.setTime("it is 10:45 PM.");
+        location.setCoordinate(256.453);
         System.out.println("\nName of Location:" + location.toString());
 
-        Location date = new Location();
-        date.setTime("it is 10:45 PM.");
-        System.out.println("\nWhat is the time? " + date.toString());
 
-        Location grid = new Location();
-        grid.setCoordinate(256.453);
-        System.out.println("\nLocation on grid:" + grid.toString());
+      
 
         Map map = new Map();
         map.setRowCount(0.5);
+        map.setColumnCount(0.6);
         System.out.println("\nnumber of rows:" + map.toString());
 
-        Map life = new Map();
-        life.setColumnCount(0.6);
-        System.out.println("\nnumber of columns:" + life.toString());
-
+    
         Scene scene = new Scene();
         scene.setDescription("meet Jennifer Lawrence");
+        scene.setDisplaySymbol("JL");
         System.out.println("\nWhen you arrive, you " + scene.toString());
 
-        Scene symbol = new Scene();
-        symbol.setDisplaySymbol("JL");
-        System.out.println("\ninitials of Jennifer Lawrence: " + symbol.toString());
+     
 
         RandomScene randomscene = new RandomScene();
         randomscene.setTitle("Jennifer Anniston");
@@ -95,33 +76,23 @@ public class BYUIDo implements Serializable {
 
         Girl old = new Girl();
         old.setAge(24);
+        old.setInterest("likes sports");
         System.out.println("\nAge of the girl: " + old.toString());
 
-        Girl hobby = new Girl();
-        hobby.setInterest("likes sports");
-        System.out.println("\nFavorite Hobby: " + hobby.toString());
 
         Friend grade = new Friend();
         grade.setGrade("sophmore");
+        grade.setAge(19);
         System.out.println("\nWhat grade is he? " + grade.toString()
         );
-
-        Friend age = new Friend();
-        age.setAge(19);
-        System.out.println("\nWhat age is he? " + age.toString()
-        );
-
+        
         DateScene place = new DateScene();
         place.setTitle("The Craze");
+        place.setInteraction("You go bowling!");
+        place.setFoodbased("true");
         System.out.println("\nWhere shall we go for our date? " + place.toString());
+       
 
-        DateScene interact = new DateScene();
-        interact.setInteraction("You go bowling!");
-        System.out.println("\nWhat shall we do? " + interact.toString());
-
-        DateScene food = new DateScene();
-        food.setFoodbased("true");
-        System.out.println("\nCan we eat here? " + food.toString());
 
     }
 }
