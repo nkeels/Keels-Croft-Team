6fa1b7ed22567143d72d9edae394b21a92908fec
croftsr <cro11007@byui.edu>
2016-10-08 12:34:25 -0600

    added extra code to rid ourselves of null value returns.

--- BYUI-Do/src/byui/pkgdo/BYUIDo.java
+++ BYUI-Do/src/byui/pkgdo/BYUIDo.java
@@ -75,18 +75,30 @@ public class BYUIDo implements Serializable {
         System.out.println("\nWhat happened:" + randomscene.toString());
 
         Girl old = new Girl();
+        old.setCharName("Jamie Snow");
+        old.setStress(12);
+        old.setConfide(64);
+        old.setGender("Female");
+        old.setPersonalityQual(100);
         old.setAge(24);
         old.setInterest("likes sports");
         System.out.println("\nAge of the girl: " + old.toString());
 
 
         Friend grade = new Friend();
+        old.setCharName("Samson Grayson");
+        old.setStress(95);
+        old.setConfide(50);
+        old.setGender("Male");
+        old.setPersonalityQual(12);
         grade.setGrade("sophmore");
         grade.setAge(19);
         System.out.println("\nWhat grade is he? " + grade.toString()
         );
         
         DateScene place = new DateScene();
+        scene.setDescription("You go on a date with Jennifer Lawrence");
+        scene.setDisplaySymbol("W");
         place.setTitle("The Craze");
         place.setInteraction("You go bowling!");
         place.setFoodbased("true");
