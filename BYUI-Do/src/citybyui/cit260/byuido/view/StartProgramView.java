f561ec23779a47efb534cfe494f1901d5440ead4
croftsr <cro11007@byui.edu>
2016-10-26 13:23:09 -0600

    addition of text for welcome banner

--- BYUI-Do/src/citybyui/cit260/byuido/view/StartProgramView.java
+++ BYUI-Do/src/citybyui/cit260/byuido/view/StartProgramView.java
@@ -23,12 +23,20 @@ public class StartProgramView {
     private void displayBanner() {
         
         System.out.println(
-        "\n*****************************************"
-      + "\n*                                        "
-      + "\n* This is the game of BYU-I DO!!!        " 
-      + "\n* In this game you will help your character"
-      + "\n* experience the joys of dating in Rexburg."
-      + "\n*                                        "          
-        );
+        "\n**********************************************"
+      + "\n*                                             "
+      + "\n* This is the game of BYU-I DO!!!             " 
+      + "\n* In this game you will help your character   "
+      + "\n* experience the joys of dating in Rexburg.   "
+      + "\n*                                             "          
+      + "\n* In order to find your eternal spouse,       "  
+      + "\n* You will begin by finding the girl of       "  
+      + "\n* your dreams at one of many Apartments.      "  
+      + "\n* You will then work to improve your relationship "  
+      + "\n* and build trust by going on dates and       " 
+      + "\n* spending time with her. Eventually, you'll  "  
+      + "\n* want to ask her to join in matrimony.       " 
+      + "\n* When you've won her hand in marriage,       "
+      + "\n* You will be prepared for eternal happiness! "  );
     }
 }
