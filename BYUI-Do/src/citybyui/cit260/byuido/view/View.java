50d553810b8765b9d85bf2f72311be743e630ce8
croftsr <cro11007@byui.edu>
2016-11-07 19:20:50 -0700

    inheritaence and other classes

---
+++ BYUI-Do/src/citybyui/cit260/byuido/view/View.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package citybyui.cit260.byuido.view;
+
+/**
+ *
+ * @author Scotty
+ */
+public abstract class View implements ViewInterface{
+    
+    protected String displayMessage;
+    
+    public View() {
+}
+    public View(String message) {
+        this.displayMessage = message;
+    }
+    
+    @Override
+    // page 8/14, 2nd illustration
+}
+
