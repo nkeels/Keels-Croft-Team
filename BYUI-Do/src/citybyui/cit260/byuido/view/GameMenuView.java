0372b5e22e079e1236d1d0e35ef93a1f5a021aca
Nick <Nick@LAPTOP-MFGV8EVG>
2016-11-23 22:28:21 -0700

    suh dude

--- BYUI-Do/src/citybyui/cit260/byuido/view/GameMenuView.java
+++ BYUI-Do/src/citybyui/cit260/byuido/view/GameMenuView.java
@@ -87,7 +87,7 @@ public class GameMenuView extends View {
 //this just needs to be aligned, essentially. You'll need to submit it though        
         System.out.println("\n Where will you go?");
 
-        System.out.println(" |----0------1------2------3------4");
+        System.out.println(" |--0------1------2------3------4");
         for ( int row = 0; row < locations.length; row++ ) {
             System.out.println("\n" + row + "|------|------|------|------|------|");
             
