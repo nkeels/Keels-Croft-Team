9d79a4237bb5c6d78e73046fe942c3ad2963b973
croftsr <cro11007@byui.edu>
2016-11-30 13:35:44 -0700

    11/30/2016

--- BYUI-Do/src/byui/cit260/BYUIDO/control/StatControl.java
+++ BYUI-Do/src/byui/cit260/BYUIDO/control/StatControl.java
@@ -46,20 +46,6 @@ public int relationPoint(int fun,int date,int days) throws StatControlException 
     return points;
     }
 
-    public int relationPoint(int fun, int date, int days) {
-        if (fun < 0) {
-            return -1;
-        }
-        if (date < 0) {
-            return -1;
-        }
-        if (days < 0) {
-            return -1;
-        }
-
-        int points = ((fun * date) + days);
-        return points;
-    }
 
     public int proposal(int points, int trust, int stress) throws StatControlException {
         if (points < 0) {
