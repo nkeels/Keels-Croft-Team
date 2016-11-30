178781374b090ee6d97e4b605a0ffd11b6df6984
Nick <Nick@LAPTOP-MFGV8EVG>
2016-11-30 13:08:44 -0700

    11/29/2016 Working on team Assignment

--- BYUI-Do/src/byui/cit260/BYUIDO/control/StatControl.java
+++ BYUI-Do/src/byui/cit260/BYUIDO/control/StatControl.java
@@ -13,14 +13,6 @@ public class StatControl {
     
     public int dateAgree(int stress,int charisma,int confide) {
         
-//        if (stress < 0)
-//            return -1
-//                    if (confide < 0)
-//            return -1
-//                    if (charisma < 0)
-//            return -1
-//            result = (charisma + confide - stress)
-//            return result;
 
     if (stress < 0) {
             return -1;
