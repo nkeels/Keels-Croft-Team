e17b237e8bdac52eba7a053173d4d10f76707d78
Nick <Nick@LAPTOP-MFGV8EVG>
2016-11-30 13:10:46 -0700

    11/29/2016 Working on individual Assignment

---
+++ BYUI-Do/src/citbyui/cit260/BYUIDO/exceptions/StatControlException.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package citbyui.cit260.BYUIDO.exceptions;
+
+/**
+ *
+ * @author Nick
+ */
+public class StatControlException extends Exception {
+
+    public StatControlException() {
+    }
+
+    public StatControlException(String message) {
+        super(message);
+    }
+
+    public StatControlException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public StatControlException(Throwable cause) {
+        super(cause);
+    }
+
+    public StatControlException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+    
+    
+}
