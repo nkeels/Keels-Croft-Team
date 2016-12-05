bb953e3c16f75bad8ffbe59303bdc5e813729c17
Nick <Nick@LAPTOP-MFGV8EVG>
2016-12-05 14:09:32 -0700

    11/29/2016 Working on individual Assignment

--- BYUI-Do/src/byui/cit260/BYUIDO/control/GirlControl.java
+++ BYUI-Do/src/byui/cit260/BYUIDO/control/GirlControl.java
@@ -26,7 +26,7 @@ public class GirlControl {
         {
             for (int j = i; j > 0; j--) {
                 if (list[j].getDescription().compareTo(list[j - 1].getDescription()) < 0) {
-                    Girl temp = list[5];
+                    Girl temp = list[6];
                     list[j] = list[j - 1];
                     list[j - 1] = temp;
                 }
--- BYUI-Do/src/citybyui/cit260/byuido/view/SpiritPromptView.java
+++ BYUI-Do/src/citybyui/cit260/byuido/view/SpiritPromptView.java
@@ -18,22 +18,24 @@ public class SpiritPromptView extends View {
     public String message;
 
     public SpiritPromptView() {
-        super("\n---------------------------------------------------------------"
-                + "\nYou wake up right when the teacher declares the end of your class on Marriage."
-                + "\n You pack up your books and walk out of the classroom. totally set for grabbing"
-                + "\n some lunch at your apartment, you meet up with one of your roommates. Your roommate"
-                + "\n starts talking about how his relationship with his girlfriend is going well and that"
-                + "\n it'll be nice once they finally get engaged and married. "
-                + "\n                                                                                  "
-                + "\n\"It would be nice to find someone to settle down...\" your thoughts float in your mind as you think."
-                + "\n Your roommate notices your glazed look and smirks. \"You know, I have a list cute girls who are available if you want to take a look...\""
-                + "\n You think for a moment on whether or not to take a look at the list."
-                + "\n"
-                + "\n---------------------------------------------------------------"
-                + "\n|G - see the list of girls                                    |"
-                + "\n|I - Ignore his advice                                        |"
-                + "\n|Q - Quit                                                     |"
-                + "\n---------------------------------------------------------------");
+        super   ( "\n|------------------------------------------------------------------------------------|"
+                + "\n|Welcome!!!                                                                          |"
+                + "\n|------------------------------------------------------------------------------------|"
+                + "\n|You wake up right when the teacher declares the end of your class on Marriage.      |"
+                + "\n|You pack up your books and walk out of the classroom. totally set for grabbing      |"
+                + "\n|some lunch at your apartment, you meet up with one of your roommates. Your roommate |"
+                + "\n|starts talking about how his relationship with his girlfriend is going well and that|"
+                + "\n|it'll be nice once they finally get engaged and married.                            |"
+                + "\n|                                                                                    |"
+                + "\n|\"It would be nice to find someone to settle down...\" your thoughts float in your  |"
+                + "\n|Your roommate notices your glazed look and smirks. \"You know, I have a list cute   |"
+                + "\n|girls who are available if you want to take a look...\".You think for a moment on   |"
+                + "\n|whether or not to take a look at the list.                                          |"
+                + "\n-------------------------------------------------------------------------------------|"
+                + "\n|G - see the list of girls                                                           |"
+                + "\n|I - Ignore his advice                                                               |"
+                + "\n|Q - Quit                                                                            |"
+                + "\n-------------------------------------------------------------------------------------|");
 
     }
 
